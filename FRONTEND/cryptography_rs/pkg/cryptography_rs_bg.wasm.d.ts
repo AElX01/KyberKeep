95e6b626d8df60277b76c8cec94dd238500a8887
AElX01 <daniel.juarez@iteso.mx>
2025-05-12 08:16:08 -0600

    added pkg directory

---
+++ FRONTEND/cryptography_rs/pkg/cryptography_rs_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const chacha20poly1305_encrypt: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const chacha20poly1305_decrypt: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const derive_key_from_master_password: (a: number, b: number, c: number) => [number, number, number, number];
+export const derive_key_from_master_password_with_defined_salt: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const generate_auth_hmac: (a: number, b: number, c: number, d: number) => [number, number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
